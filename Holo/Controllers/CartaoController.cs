ed3aaf5f6f41d82bebb33eb6adb84fab4e11bec6
joao barreto <joao.barreto@cg.df.gov.br>
2023-06-29 02:40:22 -0300

    criar cartao

---
+++ Holo/Controllers/CartaoController.cs
@@ -0,0 +1,47 @@
+﻿using Holo.Domain.config;
+using Holo.Domain.Entities;
+using Holo.Models.Cartao;
+using Holo.Models.Orcamento;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Holo.Controllers
+{
+    [Route("cartao")]
+    [ApiController]
+    public class CartaoController : ControllerBase
+    {
+        private readonly HoloDbContext _context;
+
+        public CartaoController(HoloDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult<Cartao> CriarCartao([FromBody] CriarCartao criarCartao)
+        {
+            if (criarCartao is null)
+            {
+                return BadRequest("Orcamento inválido");
+            }
+
+            Cartao novoCartao = new Cartao
+            {
+                Nome = criarCartao.Nome,
+                Numero = criarCartao.Numero,
+                CVV = criarCartao.Cvv,
+                Mes = criarCartao.Mes,
+                Ano = criarCartao.Ano,
+                UsuarioId = criarCartao.UsuarioId
+            };
+
+            _context.Cartoes.Add(novoCartao);
+            _context.SaveChanges();
+
+            return Ok(new { Id = novoCartao.Id });
+        }
+    }
+}
---
+++ Holo/Models/Cartao/CriarCartao.cs
@@ -0,0 +1,12 @@
+﻿namespace Holo.Models.Cartao
+{
+    public class CriarCartao
+    {
+        public string Numero { get; set; }
+        public int Cvv { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+        public string Nome { get; set; }
+        public int UsuarioId { get; set; }
+    }
+}
